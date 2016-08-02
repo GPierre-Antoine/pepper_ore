89928fad0409d10238b9624df30818e719fcf1cf
Pierre-Antoine Guillaume <pierreantoine.guillaume+github@gmail.com>
2016-08-02 03:40:29 +0200

    main & dom initialisation

--- main.cpp
+++ main.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
 
+
+#include "src/dom_node.h"
+
+using namespace pa::dom;
+using pa::dom::dom_node;
+
 int main ()
 {
+    dom_node d = dom_node ();
     std::cout << "Hello, World!" << std::endl;
     return 0;
 }
---
+++ src/dom_node.cpp
@@ -0,0 +1,17 @@
+//
+// Created by Pierre-Antoine on 01/08/2016.
+//
+
+#include "dom_node.h"
+using pa::dom::dom_node;
+
+
+dom_node::dom_node ()
+{
+    //TODO 01/08/2016 automatedly generated
+}
+
+bool dom_node::operator == (const dom_node & other) const
+{
+    return &other == this;
+}
