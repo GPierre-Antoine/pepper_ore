c15941d93996920305d1c0cf5515c7910ce6f1cd
Pierre-Antoine GUILLAUME <pierreantoine.guillaume+github@gmail.com>
2017-07-14 00:13:50 +0200

    basic html treatment for test purpose, functionnal

--- main.cpp
+++ main.cpp
@@ -1,10 +1,16 @@
 #include <iostream>     //cout
+#include <regex>
+
+#include "boost/filesystem.hpp"
+#include "boost/range/adaptor/reversed.hpp"
+
+
 //#include <vector>       //vector
 //#include <memory>       //shared ptr
 //#include "src/pepper/html/hr.hpp"
 //#include "src/pepper/html/img.hpp"
 //#include "src/pepper/attributes/attribute.h"
-//
+
 //using namespace SC::pepper::html;
 //using std::cout;
 //using std::endl;
@@ -12,7 +18,28 @@
 //
 //using namespace SC::pepper::attribute;
 
+namespace fs = boost::filesystem;
+
+using std::cout;
+using std::cin;
+using std::cerr;
+using std::endl;
+
+
 #include "fcgi/fcgio.h"
+
+
+//broken
+//std::vector<std::string> && filter(std::vector<std::string>::iterator begin,
+//                                   std::vector<std::string>::iterator end,
+//                                   std::function<bool(std::string)> function) {
+//    std::vector<std::string> holder;
+//    for (;begin!=end;++begin) {
+//        if (function(*begin)) holder.insert(holder.end(),*begin);
+//    }
+//    return std::move(holder);
+//}
+
 
 int main ()
 {
@@ -36,7 +63,12 @@ int main ()
 //    for (const shared_ptr<component> & it : vec)
 //        cout << it->edit () << endl;
 
+    std::locale old;
+    std::locale::global(std::locale("fr_FR.UTF-8"));
 
+    fs::path path_js(u8"js/");
+    fs::path path_css(u8"css/");
+    fs::path path_vendor(u8"vendor/");
 
     // Backup the stdio streambufs
     std::streambuf * cin_streambuf  = std::cin.rdbuf();
@@ -57,16 +89,56 @@ int main ()
         std::cout.rdbuf(&cout_fcgi_streambuf);
         std::cerr.rdbuf(&cerr_fcgi_streambuf);
 
-        std::cout << "Content-type: text/html\r\n"
-             << "\r\n"
-             << "<html>\n"
-             << "  <head>\n"
-             << "    <title>Hello, World!</title>\n"
-             << "  </head>\n"
-             << "  <body>\n"
-             << "    <h1>Hello, World! folk</h1>\n"
-             << "  </body>\n"
-             << "</html>\n";
+
+
+        std::cout << u8"Content-type: text/html" << endl << endl
+                  << u8"<!DOCTYPE html>" << endl
+                  << u8"<html>" << endl
+                  << u8"<head>" << endl
+                  << u8"<meta charset=\"UTF-16\">" << endl
+                  << u8"<title>Hello, World!</title>" << endl;
+
+
+
+        std::vector<std::string> include_all, include_js, include_css;
+        for (fs::recursive_directory_iterator it(path_vendor,fs::symlink_option::recurse);it!=fs::recursive_directory_iterator{};++it) {
+            if (fs::is_regular_file(it->path())) include_all.push_back(it->path().string());
+        }
+
+        include_css.reserve(10);
+        include_js.reserve(10);
+
+        std::regex regex_js(u8".+\\.min\\.js");
+        std::regex regex_css(u8".+\\.min\\.css");
+
+        for (auto i : include_all) {
+            if (std::regex_match(i,regex_js))
+            {
+                include_js.push_back(i);
+            }
+            if (std::regex_match(i,regex_css))
+            {
+                include_css.push_back(i);
+            }
+        }
+
+        for (const auto &js:boost::adaptors::reverse(include_js))
+            cout << u8"\t<script type=\"text/javascript\"src=\"" << js << u8"\"></script>"<<endl;
+
+        for (auto css:include_css)
+            cout << u8"\t<link rel=\"stylesheet\" type=\"text/css\" href=\""<<css<< u8"\">"<<endl;
+
+        std::cout
+                << u8"    <title>Hello, World!</title>\n"
+                << u8"  </head>\n"
+                << u8"  <body>\n"
+                << u8"  <div class=\"container\">\n"
+                << u8"    <h1>Hello, World! folk</h1>\n";
+
+        std::cout
+                << u8"  </div>"
+                << u8"  </body>\n"
+                << u8"</html>\n";
 
         // Note: the fcgi_streambuf destructor will auto flush
     }
@@ -75,5 +147,7 @@ int main ()
     std::cin.rdbuf(cin_streambuf);
     std::cout.rdbuf(cout_streambuf);
     std::cerr.rdbuf(cerr_streambuf);
+
+    std::locale::global(old);
     return 0;
 }
